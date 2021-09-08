7e2b447ca569ac88a6d80231182162f96f6e84d9
tom_glb <goodMoring_glo@atguigu.com>
2021-09-08 10:27:04 +0800

    second commit

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -6,6 +6,8 @@ package com.atguigu.git;
  */
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git1");
+        System.out.println("Hello Git2");
+
     }
 }
