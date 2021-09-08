871ae67e5ddd77932acc1ac905858f53a21fb273
tom_glb <goodMoring_glo@atguigu.com>
2021-09-08 10:28:27 +0800

    third commit

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -8,6 +8,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
-
+        System.out.println("Hello Git3");
     }
 }
