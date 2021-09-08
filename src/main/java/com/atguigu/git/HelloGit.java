ec8292a9c0257986508a1bb6e3aaa952d5ab3cd9
tom_glb <goodMoring_glo@atguigu.com>
2021-09-08 10:23:27 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zc
+ * @create 2021-09-08-10:11
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
