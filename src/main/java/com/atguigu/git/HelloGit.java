a9b826b2a5a2451df1efa23d075e3fe0d2a88e6a
tom_glb <goodMoring_glo@atguigu.com>
2021-09-08 10:40:21 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
+        System.out.println("Hello Git4 --edit by hot-fix");
     }
 }
